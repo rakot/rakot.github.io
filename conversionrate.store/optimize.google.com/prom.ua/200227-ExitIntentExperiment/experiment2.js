6c52c42921520b54f8ddebac1b224fed356c137c
SergeyVardanyan <rakot.ss@gmail.com>
2020-03-05 21:07:42 +0300

    Prom

--- conversionrate.store/optimize.google.com/prom.ua/200227-ExitIntentExperiment/experiment2.js
+++ conversionrate.store/optimize.google.com/prom.ua/200227-ExitIntentExperiment/experiment2.js
@@ -1,4 +1,7 @@
 $(function () {
+    (function(a){function d(e){0<e.clientY||(b&&clearTimeout(b),0>=a.exitIntent.settings.sensitivity?a.event.trigger("exitintent"):b=setTimeout(function(){b=null;a.event.trigger("exitintent")},a.exitIntent.settings.sensitivity))}function c(){b&&(clearTimeout(b),b=null)}var b;a.exitIntent=function(b,f){a.exitIntent.settings=a.extend(a.exitIntent.settings,f);if("enable"==b)a(window).mouseleave(d),a(window).mouseenter(c);else if("disable"==b)c(),a(window).unbind("mouseleave",d),a(window).unbind("mouseenter",
+        c);else throw"Invalid parameter to jQuery.exitIntent -- should be 'enable'/'disable'";};a.exitIntent.settings={sensitivity:300}})(jQuery);
+
     var getCookie = function(name) {
         let matches = document.cookie.match(new RegExp(
             "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -137,7 +140,10 @@ $(function () {
             if(timestamp > intentPopupShowTimer) {
                 // Let showup the popup
                 localStorage.setItem('intentPopupShowTimer', 'used');
-                showPopup();
+                $.exitIntent('enable');
+                $(document).bind('exitintent',function(){
+                    showPopup();
+                });
             }
         }
     }, 100);
