f5bd55f76b826693c1fe6d7303c9041f8d46640b
SergeyVardanyan <rakot.ss@gmail.com>
2020-04-28 20:59:32 +0300

    Depositfiles

--- conversionrate.store/optimize.google.com/depositphotos.com/LiveChat/variation.js
+++ conversionrate.store/optimize.google.com/depositphotos.com/LiveChat/variation.js
@@ -1,15 +1,19 @@
 Backbone.$(function () {
     var $ = Backbone.$;
-    var wrapper = $('.top-button-wrapper');
-
-    wrapper.prepend('<div class="live-chat-floating-duplicate-button">Live chat</div>');
-    $('.live-chat-floating-duplicate-button').click(function (e) {
-        e.stopImmediatePropagation();
-        $('.cmp-livechat-online').click();
-        return false;
-    });
 
     setInterval(function () {
+        if($('.top-button-wrapper').length && $('.top-button-wrapper .live-chat-floating-duplicate-button').length === 0) {
+            var wrapper = $('.top-button-wrapper');
+            wrapper.prepend('<div class="live-chat-floating-duplicate-button">Live chat</div>');
+            wrapper.prepend('<div class="live-chat-floating-duplicate-button">Live chat</div>');
+            $('.live-chat-floating-duplicate-button').click(function (e) {
+                e.stopImmediatePropagation();
+                $('.cmp-livechat-online').click();
+                return false;
+            });
+        }
+
+
         if($('.modal-container:visible').length && $('.modal-container .live-chat-floating-duplicate-button').length === 0) {
             $('.modal-container').append('<div class="live-chat-floating-duplicate-button">Live chat</div>');
             $('.modal-container .live-chat-floating-duplicate-button').click(function (e) {
