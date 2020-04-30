c083e9f97f63b216591e5acb3c031870a1d462a7
SergeyVardanyan <rakot.ss@gmail.com>
2020-04-30 15:21:02 +0300

    Depositfiles

--- conversionrate.store/optimize.google.com/depositphotos.com/LiveChat/variation.js
+++ conversionrate.store/optimize.google.com/depositphotos.com/LiveChat/variation.js
@@ -2,9 +2,18 @@ Backbone.$(function () {
     var $ = Backbone.$;
 
     setInterval(function () {
+        if($('section.cookie-bar:visible').length) {
+            if($('body.experiment-cookie-bar-visible').length === 0) {
+                $('body').addClass('experiment-cookie-bar-visible');
+            }
+        } else {
+            if($('body.experiment-cookie-bar-visible').length) {
+                $('body').removeClass('experiment-cookie-bar-visible');
+            }
+        }
+
         if($('.top-button-wrapper').length && $('.top-button-wrapper .live-chat-floating-duplicate-button').length === 0) {
             var wrapper = $('.top-button-wrapper');
-            wrapper.prepend('<div class="live-chat-floating-duplicate-button">Live chat</div>');
             wrapper.prepend('<div class="live-chat-floating-duplicate-button">Live chat</div>');
             $('.live-chat-floating-duplicate-button').click(function (e) {
                 e.stopImmediatePropagation();
@@ -12,7 +21,6 @@ Backbone.$(function () {
                 return false;
             });
         }
-
 
         if($('.modal-container:visible').length && $('.modal-container .live-chat-floating-duplicate-button').length === 0) {
             $('.modal-container').append('<div class="live-chat-floating-duplicate-button">Live chat</div>');
