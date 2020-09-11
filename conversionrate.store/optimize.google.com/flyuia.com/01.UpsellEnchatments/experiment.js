bd0842c232c3cb0b3b51bd08ea3e9894c6773e8b
rakot <rakot.ss@gmail.com>
2020-09-11 16:43:51 +0300

    flyua

--- conversionrate.store/optimize.google.com/flyuia.com/01.UpsellEnchatments/experiment.js
+++ conversionrate.store/optimize.google.com/flyuia.com/01.UpsellEnchatments/experiment.js
@@ -17,6 +17,7 @@ el.addEventListener('load', function() {
     });
 
     let textData = {
+        'Мне не нужен регистрируемый багаж': 'Мне не нужен регистрируемый багаж',
         'Добавьте багаж с 50% скидкой!':'Добавьте багаж с 50% скидкой!',
         'На стойке регистрации стоимость будет в 2 раза больше!':'На стойке регистрации стоимость будет в 2 раза больше!',
         'Включено:':'Включено:',
@@ -34,13 +35,15 @@ el.addEventListener('load', function() {
         '(Быстрый доступ к центральному проходу)':'(Быстрый доступ к центральному проходу)',
         'Больше места для ног':'Больше места для ног',
         '(Вы получаете +70 см пространства для ног)':'(Вы получаете +70 см пространства для ног)',
-        'Место в передней части самолета':'(Более быстрая посадка и выход)',
+        'Место в передней части самолета':'Место в передней части самолета',
+        '(Более быстрая посадка и выход)':'(Более быстрая посадка и выход)',
         'Сидите вместе. Выбирайте удобные места рядом с близкими.':'Сидите вместе. Выбирайте удобные места рядом с близкими.',
         'Бесплатно. Выбор места входит в ваш тариф':'Бесплатно. Выбор места входит в ваш тариф'
     };
 
     if($('.control-item.change-language .label').text() === 'English') {
         textData = {
+            'Мне не нужен регистрируемый багаж': 'I do not need to check in luggage',
             'Добавьте багаж с 50% скидкой!':'Add luggage with 50% discount',
             'На стойке регистрации стоимость будет в 2 раза больше!':'The price for additioal luggage at registration desk will be 100% higher.',
             'Включено:':'Included:',
@@ -69,17 +72,41 @@ el.addEventListener('load', function() {
     setInterval(function () {
         if($('.luggage-content-not-included-caption').length === 0) {
             let luggage_container = $('app-ancillary-luggage-view > .included-in-fare');
+            let add_luggage_checkbox = false;
             luggage_container.find('i.icon-info').remove();
             let luggage_content = luggage_container.children('div:first');
             let luggage_contents = luggage_content.contents();
             let luggage_part1 = $('<div class="luggage-content"></div>');
             luggage_part1.append(luggage_contents[0]);
             luggage_part1.append($('<div class="luggage-content-blue-button"></div>').append(luggage_contents[1]));
+            if(luggage_part1.find('div:contains("Not Included")').length || luggage_part1.find('div:contains("Не включен")').length) {
+                add_luggage_checkbox = true;
+            }
+
             let luggage_part2 = $('<div class="luggage-content"></div>');
-            luggage_part2.append('<div class="luggage-content-not-included-caption">Включено:</div>');
+            luggage_part2.append('<div class="luggage-content-not-included-caption">'+textData['Включено:']+'</div>');
             luggage_part2.append($('<div class="luggage-content-transparent-button"></div>').append(luggage_contents[3]));
             luggage_content.html('').append($('<div class="luggage-content-container"></div>').append(luggage_part1).append(luggage_part2));
             luggage_container.after('<div class="luggage-warning-container"><div class="luggage-warning-content"><b>'+textData['Добавьте багаж с 50% скидкой!']+'</b> '+textData['На стойке регистрации стоимость будет в 2 раза больше!']+'</div></div>');
+            if(add_luggage_checkbox) {
+                $('.luggage-warning-container').after('<div class="luggage-warning-checkbox-wrapper"><div class="luggage-warning-checkbox"><input id="luggage-warning-checkbox" type="checkbox"><label for="luggage-warning-checkbox">'+textData['Мне не нужен регистрируемый багаж']+'</label></div></div>')
+                // $('.ancillary-catalog--go-to-payment button').click(function (e) {
+                //     if($('.ancillary-bag-container .ancillary-counter-count').text() === '00') {
+                //         if($('.insurance-terms-agreement input').is(':checked')) {
+                //             $('.insurance-terms-agreement input').click();
+                //             setTimeout(function () {
+                //                 $('.insurance-terms-agreement input').click();
+                //             }, 500);
+                //         }
+                //         $([document.documentElement, document.body]).animate({
+                //             scrollTop: $(".luggage-warning-checkbox-wrapper").offset().top
+                //         }, 1000);
+                //         if($('#mat-checkbox-2-input').is(':checked'))
+                //         e.stopImmediatePropagation();
+                //         return false;
+                //     }
+                // });
+            }
         }
 
         if($('.ancillary-service--header-info.ts-h4').length === 0) {
