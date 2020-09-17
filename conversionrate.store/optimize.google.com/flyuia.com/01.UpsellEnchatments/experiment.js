8f058fcc09c84d7b9b6c98f60ae61dcf81136a06
rakot <rakot.ss@gmail.com>
2020-09-17 11:47:42 +0300

    flyua

--- conversionrate.store/optimize.google.com/flyuia.com/01.UpsellEnchatments/experiment.js
+++ conversionrate.store/optimize.google.com/flyuia.com/01.UpsellEnchatments/experiment.js
@@ -16,6 +16,22 @@ el.addEventListener('load', function() {
         'eventNonInteraction': 1
     });
 
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1657822,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    window.hj = window.hj || function(){(hj.q = hj.q || []).push(arguments)};
+    try {
+        hj('trigger', 'upsell_exp');
+    }
+    catch(e) {}
+
+
+
     let textData = {
         'Мне не нужен регистрируемый багаж': 'Мне не нужен регистрируемый багаж',
         'Добавьте багаж с 50% скидкой!':'Добавьте багаж с 50% скидкой!',
