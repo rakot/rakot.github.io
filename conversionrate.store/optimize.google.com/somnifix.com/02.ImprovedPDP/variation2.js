aeaf6fc8ef52e5c4e96f412fb2ae56476bb73ac6
rakot <rakot.ss@gmail.com>
2020-09-24 18:48:47 +0300

    flyua

--- conversionrate.store/optimize.google.com/somnifix.com/02.ImprovedPDP/variation2.js
+++ conversionrate.store/optimize.google.com/somnifix.com/02.ImprovedPDP/variation2.js
@@ -1,53 +1,4 @@
 $(function () {
-    $('#template-product .purchase-details .button--add-to-cart span').text('checkout with credit card');
-    $('#template-product .purchase-details .button--add-to-cart').append('<div class="notify-one-step-away"><div class="notify-one-step-away-wrapper">' +
-        '<div class="notify-one-step-away-wrapper-green">You are one step away <br> from significantly improving your sleep</div>' +
-        '<div class="notify-one-step-away-wrapper-gray">' +
-        '<span class="notify-one-step-away-wrapper-flag">Made in USA - top quality</span>' +
-        '<span class="notify-one-step-away-wrapper-li">30 day money back guarantee</span>' +
-        '<span class="notify-one-step-away-wrapper-li">60,000 of grateful customers</span>' +
-        '<span class="notify-one-step-away-wrapper-li">99% customer satisfaction rate</span>' +
-        '</div>' +
-        '</div></div>');
-
-    $('#template-product .swatch--1-pack').next().find('.on-title').before('<div class="on-title-details">28 strips — 28 days</div>');
-    $('#template-product .swatch--3-pack').next().find('.on-title').before('<div class="on-title-details">84 strips — 84 days</div>');
-    $('#template-product .swatch--3-pack-auto-ship').next().find('.on-title').before('<div class="on-title-details">84 strips — 84 days</div>');
-
-    let notify_one_step_away = $('.notify-one-step-away');
-
-    $('#template-product  .purchase-details').hover(function () {
-        notify_one_step_away.removeAttr('style');
-    });
-
-    $('#template-product  .on-payment-wrapper img').hover(function () {
-        let self = $(this).parent();
-        if(self.hasClass('on-item on-item-1')) {
-            notify_one_step_away.css({
-                'margin-left': '-'+Math.round(self.width()/2)+'px',
-                'bottom': (self.height()*2+60).toString()+'px'
-            });
-        }
-        if(self.hasClass('on-item on-item-2')) {
-            notify_one_step_away.css({
-                'margin-left': Math.round(self.width()/2)+'px',
-                'bottom': (self.height()*2+60).toString()+'px'
-            });
-        }
-        if(self.hasClass('on-item on-item-3')) {
-            notify_one_step_away.css({
-                'margin-left': '-'+Math.round(self.width()/2)+'px',
-                'bottom': (self.height()+50).toString()+'px'
-            });
-        }
-        if(self.hasClass('on-item on-item-4')) {
-            notify_one_step_away.css({
-                'margin-left': Math.round(self.width()/2)+'px',
-                'bottom': (self.height()+50).toString()+'px'
-            });
-        }
-    });
-
     $('h1:contains("How and why SomniFix works")').parents('.shogun-root').prepend('<div class="recomended-by-medical-professional container">' +
         '<h3>Recommended by Medical Professionals</h3>' +
         '<div class="recomended-by-medical-professional-wrapper">' +
@@ -61,7 +12,7 @@ $(function () {
         'Having a dry mouth can cause more cavities, gum disease, and bad breath."</div>' +
         '</div>' +
         '</div>' +
-        '</div>')
+        '</div>');
 });
 
 
