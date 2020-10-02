f6b0329a329c035fe35bb84c119cdcd3655f0bd5
rakot <rakot.ss@gmail.com>
2020-10-02 15:49:39 +0300

    deposit

---
+++ conversionrate.store/optimize.google.com/depositphotos.com/04.СheckoutTrustBlocks/variation.js
@@ -0,0 +1,8 @@
+Backbone.$(function () {
+    var $ = Backbone.$;
+    $('.billing-page__cell:last').append('<div class="checkout-trust-blocks">' +
+        '<div class="checkout-trust-blocks-1"></div>' +
+        '<div class="checkout-trust-blocks-2"></div>' +
+        '<a href="https://trustpilot.com/review/www.depositphotos.com" target="_blank" class="checkout-trust-blocks-3"></a>' +
+        '</div>');
+});
