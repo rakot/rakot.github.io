e7ffe08f84b63a3beeaa4a08278322271555883f
rakot <rakot.ss@gmail.com>
2020-10-02 16:20:27 +0300

    flyua

--- conversionrate.store/optimize.google.com/depositphotos.com/04.СheckoutTrustBlocks/variation.js
+++ conversionrate.store/optimize.google.com/depositphotos.com/04.СheckoutTrustBlocks/variation.js
@@ -1,8 +1,13 @@
 Backbone.$(function () {
     var $ = Backbone.$;
-    $('.billing-page__cell:last').append('<div class="checkout-trust-blocks">' +
-        '<div class="checkout-trust-blocks-1"></div>' +
-        '<div class="checkout-trust-blocks-2"></div>' +
-        '<a href="https://trustpilot.com/review/www.depositphotos.com" target="_blank" class="checkout-trust-blocks-3"></a>' +
-        '</div>');
+
+    setInterval(function () {
+        if($('.checkout-trust-blocks').length === 0) {
+            $('.billing-page__cell:last').append('<div class="checkout-trust-blocks">' +
+                '<div class="checkout-trust-blocks-1"></div>' +
+                '<div class="checkout-trust-blocks-2"></div>' +
+                '<a href="https://trustpilot.com/review/www.depositphotos.com" target="_blank" class="checkout-trust-blocks-3"></a>' +
+                '</div>');
+        }
+    }, 100);
 });
