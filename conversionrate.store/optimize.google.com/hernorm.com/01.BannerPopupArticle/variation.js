5237fc71161410c89eafcddee822c9d44479e68f
rakot <rakot.ss@gmail.com>
2020-10-16 09:41:43 +0300

    flyua

--- conversionrate.store/optimize.google.com/hernorm.com/01.BannerPopupArticle/variation.js
+++ conversionrate.store/optimize.google.com/hernorm.com/01.BannerPopupArticle/variation.js
@@ -77,11 +77,11 @@
     };
 
     let markup_content = {
-        headline: 'How to find out if<br>He is cheating on you?',
+        headline: 'How to find out if<br>he is cheating on you?',
         sub_headline: 'This <span>online tool</span> can legally and anonymously check:',
         list_items: '',
         list_items_text: [
-            'Who He’s been contacting frequently?',
+            'Who he’s been contacting frequently?',
             'What contact details he is using',
             'Did he download dating apps',
             'Is he married or divorced',
@@ -89,7 +89,7 @@
             'And a lot more...',
         ],
         list_item_mark: '<svg width="20" height="16" viewBox="0 0 20 16" fill="none"><path d="M19.7071 0.949099C19.3166 0.558552 18.6835 0.558552 18.2929 0.949099L6.31228 12.9298L1.70713 8.32469C1.31662 7.93414 0.683496 7.93418 0.29291 8.32469C-0.0976367 8.71519 -0.0976367 9.34832 0.29291 9.73887L5.60518 15.0511C5.99557 15.4416 6.62916 15.4413 7.01939 15.0511L19.7071 2.36332C20.0977 1.97281 20.0976 1.33965 19.7071 0.949099Z" fill="#E768B5"/></svg>',
-        cancel_button_text: 'I prefer to don’t know about this',
+        cancel_button_text: 'I prefer not to know about this',
         request_button_text: 'I want to learn more about this tool',
         // request_button_link: 'https://hernorm.com/how-to-find-out-if-hes-cheating/',
         // request_button_link: 'https://www.truthfinder.com/',
@@ -402,7 +402,7 @@
     };
 
 
-    let text = '3 ways to find out if He’s cheating?';
+    let text = '3 Ways To Find Out If He’s Cheating?';
 
     let sticky_box_content = `<span class="text">${text}</span><span class="button"><svg width="10" height="16" viewBox="0 0 10 16" fill="none">
 <path d="M9.44469 7.37568L2.32449 0.255608C2.15981 0.090796 1.93997 0 1.70557 0C1.47116 0 1.25133 0.090796 1.08664 0.255608L0.562291 0.779831C0.22109 1.12142 0.22109 1.6766 0.562291 2.01767L6.5413 7.99668L0.555657 13.9823C0.390975 14.1471 0.300049 14.3668 0.300049 14.6011C0.300049 14.8357 0.390975 15.0554 0.555657 15.2203L1.08001 15.7444C1.24482 15.9092 1.46453 16 1.69893 16C1.93334 16 2.15317 15.9092 2.31785 15.7444L9.44469 8.61782C9.60976 8.45248 9.70043 8.23174 9.69991 7.99707C9.70043 7.7615 9.60976 7.54088 9.44469 7.37568Z" fill="white"/>
