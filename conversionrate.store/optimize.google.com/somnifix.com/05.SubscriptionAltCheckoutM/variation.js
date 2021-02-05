ea943776fe3e87fbef2db625b2f09b8789c229d1
rakot <rakot.ss@gmail.com>
2021-02-05 20:43:23 +0300

    somnifix

--- conversionrate.store/optimize.google.com/somnifix.com/05.SubscriptionAltCheckoutM/variation.js
+++ conversionrate.store/optimize.google.com/somnifix.com/05.SubscriptionAltCheckoutM/variation.js
@@ -1,15 +1,34 @@
 $(function () {
-    $('.on-mobile-btn-wrapper').html('<a class="button on-mob-btn yellow">Subscribe & Save<span>Auto-delivery <s>$55.97</s> $49.97</span></a>' +
-        '<a class="button on-mob-btn blue">ONE-TIME PURCHASE $55.97</span></a>');
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1271698,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+    hj('trigger', 'optimize_pdp');
 
-    $('.on-mobile-btn-wrapper a.on-mob-btn.yellow').click(function () {
-        addItemToCart(31272810676339, 1, "3", "Months", "95310");
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({
+        'event': 'event-to-ga',
+        'eventCategory': 'Exp — Subscription with alt checkout',
+        'eventAction': 'loaded'
     });
 
-    $('.on-mobile-btn-wrapper a.on-mob-btn.blue').click(function () {
-        $('#mobile-header .on-button-get-sominifix-open').click();
-        // addItemToCart(32115046056051, 1);
-    });
+
+    // $('.on-mobile-btn-wrapper').html('<a class="button on-mob-btn yellow">Subscribe & Save<span>Auto-delivery <s>$55.97</s> $49.97</span></a>' +
+    //     '<a class="button on-mob-btn blue">ONE-TIME PURCHASE $55.97</span></a>');
+    //
+    // $('.on-mobile-btn-wrapper a.on-mob-btn.yellow').click(function () {
+    //     addItemToCart(31272810676339, 1, "3", "Months", "95310");
+    // });
+    //
+    // $('.on-mobile-btn-wrapper a.on-mob-btn.blue').click(function () {
+    //     $('#mobile-header .on-button-get-sominifix-open').click();
+    //     // addItemToCart(32115046056051, 1);
+    // });
 
     let activeForm = $('.shopify-product-form:first');
 
@@ -19,6 +38,12 @@ $(function () {
         '</div>');
 
     $('.slider-12-weeks .button.on-mob-btn.yellow').click(function () {
+        window.dataLayer = window.dataLayer || [];
+        dataLayer.push({
+            'event': 'event-to-ga',
+            'eventCategory': 'Exp — Subscription with alt checkout',
+            'eventAction': 'click on button Subscribe and Save'
+        });
         addItemToCart(31272810676339, 1, "3", "Months", "95310");
     });
 
@@ -29,6 +54,12 @@ $(function () {
 
 
     $('.slider-12-months .button.on-mob-btn.yellow').click(function () {
+        window.dataLayer = window.dataLayer || [];
+        dataLayer.push({
+            'event': 'event-to-ga',
+            'eventCategory': 'Exp — Subscription with alt checkout',
+            'eventAction': 'click on button Subscribe and Save'
+        });
         addItemToCart(32190023958643, 1, "12", "Months", "95310");
     });
 
